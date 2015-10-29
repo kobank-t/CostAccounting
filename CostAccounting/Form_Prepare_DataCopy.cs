f4ad8b74ce8464b5361d7ba2e57e64d17ba12518
takashi1520 <takashi1520@gmail.com>
2015-10-29 12:34:05 +0900

    データコピー機能を作成中。DB定義変更前のコミット

--- CostAccounting/Form_Prepare_DataCopy.cs
+++ CostAccounting/Form_Prepare_DataCopy.cs
@@ -94,77 +94,154 @@ namespace CostAccounting
             //---------------------------------------------------------
             var target01 = from t in context.CostMngTotal
                            where t.year.Equals(srcYear)
-                              //&& t.category.Equals((int)Const.CATEGORY_TYPE.Actual)
+                              && t.category.Equals((int)Const.CATEGORY_TYPE.Actual)
                            select t;
             foreach (var data in target01.ToList())
             {
                 foreach (int category in Enum.GetValues(typeof(Const.CATEGORY_TYPE)))
                 {
-                    MessageBox.Show(category + "");
+                    var entity = DeepCopyHelper.DeepCopy<CostMngTotal>(data);
+                    entity.year = Const.TARGET_YEAR;
+                    entity.category = category;
+                    entity.update_user = string.Concat(SystemInformation.ComputerName, "/", SystemInformation.UserName);
+                    entity.update_date = DateTime.Now;
+                    context.CostMngTotal.Add(entity);
                 }
-                //var entity = DeepCopyHelper.DeepCopy<CostMngTotal>(data);
-                //entity.year = Const.TARGET_YEAR;
-                //entity.update_user = string.Concat(SystemInformation.ComputerName, "/", SystemInformation.UserName);
-                //entity.update_date = DateTime.Now;
-
-
-
-                //context.CostMngTotal.Add(entity);
             }
 
             ////---------------------------------------------------------
-            //var target02 = from t in context.Divergence
-            //               where t.year.Equals(srcYear)
-            //               select t;
-            //context.Divergence.AddRange(target02);
+            var target02 = from t in context.Divergence
+                           where t.year.Equals(srcYear)
+                           select t;
+            foreach (var data in target02.ToList())
+            {
+                var entity = DeepCopyHelper.DeepCopy<Divergence>(data);
+                entity.year = Const.TARGET_YEAR;
+                entity.update_user = string.Concat(SystemInformation.ComputerName, "/", SystemInformation.UserName);
+                entity.update_date = DateTime.Now;
+                context.Divergence.Add(entity);
+            }
 
             ////---------------------------------------------------------
-            //var target03 = from t in context.Fare
-            //               where t.year.Equals(srcYear)
-            //               select t;
-            //context.Fare.AddRange(target03);
+            var target03 = from t in context.Fare
+                           where t.year.Equals(srcYear)
+                           select t;
+            foreach (var data in target03.ToList())
+            {
+                var entity = DeepCopyHelper.DeepCopy<Fare>(data);
+                entity.year = Const.TARGET_YEAR;
+                entity.price_budget = data.price_actual;
+                entity.update_user = string.Concat(SystemInformation.ComputerName, "/", SystemInformation.UserName);
+                entity.update_date = DateTime.Now;
+                context.Fare.Add(entity);
+            }
 
-            ////---------------------------------------------------------
-            //var target04 = from t in context.Item
-            //               where t.year.Equals(srcYear)
-            //               select t;
-            //context.Item.AddRange(target04);
+            //---------------------------------------------------------
+            var target04 = from t in context.Item
+                           where t.year.Equals(srcYear)
+                           select t;
+            foreach (var data in target04.ToList())
+            {
+                var entity = DeepCopyHelper.DeepCopy<Item>(data);
+                entity.year = Const.TARGET_YEAR;
+                entity.update_user = string.Concat(SystemInformation.ComputerName, "/", SystemInformation.UserName);
+                entity.update_date = DateTime.Now;
+                context.Item.Add(entity);
+            }
 
-            ////---------------------------------------------------------
-            //var target05 = from t in context.Machine
-            //               where t.year.Equals(srcYear)
-            //               select t;
-            //context.Machine.AddRange(target05);
+            //---------------------------------------------------------
+            var target05 = from t in context.Machine
+                           where t.year.Equals(srcYear)
+                           select t;
+            foreach (var data in target05.ToList())
+            {
+                var entity = DeepCopyHelper.DeepCopy<Machine>(data);
+                entity.year = Const.TARGET_YEAR;
+                entity.rate_budget = data.rate_actual;
+                entity.update_user = string.Concat(SystemInformation.ComputerName, "/", SystemInformation.UserName);
+                entity.update_date = DateTime.Now;
+                context.Machine.Add(entity);
+            }
 
-            ////---------------------------------------------------------
-            //var target06 = from t in context.Material
-            //               where t.year.Equals(srcYear)
-            //               select t;
-            //context.Material.AddRange(target06);
+            //---------------------------------------------------------
+            var target06 = from t in context.Material
+                           where t.year.Equals(srcYear)
+                           select t;
+            foreach (var data in target06.ToList())
+            {
+                var entity = DeepCopyHelper.DeepCopy<Material>(data);
+                entity.year = Const.TARGET_YEAR;
+                entity.price_budget = data.price_actual;
+                entity.update_user = string.Concat(SystemInformation.ComputerName, "/", SystemInformation.UserName);
+                entity.update_date = DateTime.Now;
+                context.Material.Add(entity);
+            }
 
-            ////---------------------------------------------------------
-            //var target07 = from t in context.Other
-            //               where t.year.Equals(srcYear)
-            //               select t;
-            //context.Other.AddRange(target07);
+            //---------------------------------------------------------
+            var target07 = from t in context.Other
+                           where t.year.Equals(srcYear)
+                              && t.category.Equals((int)Const.CATEGORY_TYPE.Actual)
+                           select t;
+            foreach (var data in target07.ToList())
+            {
+                foreach (int category in Enum.GetValues(typeof(Const.CATEGORY_TYPE)))
+                {
+                    var entity = DeepCopyHelper.DeepCopy<Other>(data);
+                    entity.year = Const.TARGET_YEAR;
+                    entity.category = category;
+                    entity.update_user = string.Concat(SystemInformation.ComputerName, "/", SystemInformation.UserName);
+                    entity.update_date = DateTime.Now;
+                    context.Other.Add(entity);
+                }
+            }
 
-            ////---------------------------------------------------------
-            //var target08 = from t in context.Product
-            //               where t.year.Equals(srcYear)
-            //               select t;
-            //context.Product.AddRange(target08);
+            //---------------------------------------------------------
+            var target08 = from t in context.Product
+                           where t.year.Equals(srcYear)
+                              && t.category.Equals((int)Const.CATEGORY_TYPE.Actual)
+                           select t;
+            foreach (var data in target08.ToList())
+            {
+                foreach (int category in Enum.GetValues(typeof(Const.CATEGORY_TYPE)))
+                {
+                    var entity = DeepCopyHelper.DeepCopy<Product>(data);
+                    entity.year = Const.TARGET_YEAR;
+                    entity.category = category;
+                    entity.update_user = string.Concat(SystemInformation.ComputerName, "/", SystemInformation.UserName);
+                    entity.update_date = DateTime.Now;
+                    context.Product.Add(entity);
+                }
+            }
 
-            ////---------------------------------------------------------
-            //var target09 = from t in context.ProductBlend
-            //               where t.id.StartsWith(srcYear.ToString())
-            //               select t;
-            //context.ProductBlend.AddRange(target09);
+            //---------------------------------------------------------
+            var target09 = from t in context.ProductBlend
+                           where t.id.StartsWith(srcYear.ToString())
+                              && t.id.EndsWith(((int)Const.CATEGORY_TYPE.Actual).ToString())
+                           select t;
+            foreach (var data in target09.ToList())
+            {
+                foreach (int category in Enum.GetValues(typeof(Const.CATEGORY_TYPE)))
+                {
+                    var entity = DeepCopyHelper.DeepCopy<ProductBlend>(data);
+                    entity.id = "";
+                    entity.update_user = string.Concat(SystemInformation.ComputerName, "/", SystemInformation.UserName);
+                    entity.update_date = DateTime.Now;
+                    context.ProductBlend.Add(entity);
+                }
+            }
 
-            ////---------------------------------------------------------
-            //var target10 = from t in context.ProductCode
-            //               where t.year.Equals(srcYear)
-            //               select t;
-            //context.ProductCode.AddRange(target10);
+            //---------------------------------------------------------
+            var target10 = from t in context.ProductCode
+                           where t.year.Equals(srcYear)
+                           select t;
+            foreach (var data in target10.ToList())
+            {
+                var entity = DeepCopyHelper.DeepCopy<ProductCode>(data);
+                entity.year = Const.TARGET_YEAR;
+                entity.update_user = string.Concat(SystemInformation.ComputerName, "/", SystemInformation.UserName);
+                entity.update_date = DateTime.Now;
+                context.ProductCode.Add(entity);
+            }
 
             ////---------------------------------------------------------
             //var target11 = from t in context.ProductContractor
@@ -202,23 +279,50 @@ namespace CostAccounting
             //               select t;
             //context.ProductPackingFare.AddRange(target16);
 
-            ////---------------------------------------------------------
-            //var target17 = from t in context.ProductSupplier
-            //               where t.year.Equals(srcYear)
-            //               select t;
-            //context.ProductSupplier.AddRange(target17);
+            //---------------------------------------------------------
+            var target17 = from t in context.ProductSupplier
+                           where t.year.Equals(srcYear)
+                              && t.category.Equals((int)Const.CATEGORY_TYPE.Actual)
+                           select t;
+            foreach (var data in target17.ToList())
+            {
+                foreach (int category in Enum.GetValues(typeof(Const.CATEGORY_TYPE)))
+                {
+                    var entity = DeepCopyHelper.DeepCopy<ProductSupplier>(data);
+                    entity.year = Const.TARGET_YEAR;
+                    entity.category = category;
+                    entity.update_user = string.Concat(SystemInformation.ComputerName, "/", SystemInformation.UserName);
+                    entity.update_date = DateTime.Now;
+                    context.ProductSupplier.Add(entity);
+                }
+            }
 
-            ////---------------------------------------------------------
-            //var target18 = from t in context.RowMaterial
-            //               where t.year.Equals(srcYear)
-            //               select t;
-            //context.RowMaterial.AddRange(target18);
+            //---------------------------------------------------------
+            var target18 = from t in context.RowMaterial
+                           where t.year.Equals(srcYear)
+                           select t;
+            foreach (var data in target18.ToList())
+            {
+                var entity = DeepCopyHelper.DeepCopy<RowMaterial>(data);
+                entity.year = Const.TARGET_YEAR;
+                entity.price_budget = data.price_actual;
+                entity.update_user = string.Concat(SystemInformation.ComputerName, "/", SystemInformation.UserName);
+                entity.update_date = DateTime.Now;
+                context.RowMaterial.Add(entity);
+            }
 
-            ////---------------------------------------------------------
-            //var target19 = from t in context.Supplier
-            //               where t.year.Equals(srcYear)
-            //               select t;
-            //context.Supplier.AddRange(target19);
+            //---------------------------------------------------------
+            var target19 = from t in context.Supplier
+                           where t.year.Equals(srcYear)
+                           select t;
+            foreach (var data in target19.ToList())
+            {
+                var entity = DeepCopyHelper.DeepCopy<Supplier>(data);
+                entity.year = Const.TARGET_YEAR;
+                entity.update_user = string.Concat(SystemInformation.ComputerName, "/", SystemInformation.UserName);
+                entity.update_date = DateTime.Now;
+                context.Supplier.Add(entity);
+            }
         }
 
         /*************************************************************
