b42d2c5a502a73914cafca0f5cee7f9b644f250c
takashi1520 <takashi1520@gmail.com>
2015-11-02 16:11:42 +0900

    データコピーのログ出力を実�

--- CostAccounting/Form_Prepare_DataCopy.cs
+++ CostAccounting/Form_Prepare_DataCopy.cs
@@ -75,12 +75,13 @@ namespace CostAccounting
                     // 今年度のデータを削除の上、コピー元年度のデータを今年度にコピー
                     using (var context = new CostAccountingEntities())
                     {
-                        allDelete(context);
-                        allAppend(context);
+                        string deleteLog = allDelete(context);
+                        string appendLog = allAppend(context);
                         context.SaveChanges();
+
+                        Logger.Info(Message.INF003, new string[] { this.Text, Message.create(srcYear) + "削除件数{" + deleteLog + " } 登録件数{" + appendLog + "}" });
                     }
 
-                    Logger.Info(Message.INF003, new string[] { this.Text, "-" });
                     Program.MessageBoxAfter("データコピーが完了しました。");
                 }
             }
@@ -89,8 +90,10 @@ namespace CostAccounting
         /*************************************************************
          * データ登録処理
          *************************************************************/
-        private void allAppend(CostAccountingEntities context)
+        private string allAppend(CostAccountingEntities context)
         {
+            string logStr = string.Empty;
+
             int srcYear = (int)Conversion.Parse(this.srcYear.Text);
             //---------------------------------------------------------
             var target01 = from t in context.CostMngTotal
@@ -99,29 +102,33 @@ namespace CostAccounting
                            select t;
             foreach (var data in target01.ToList())
             {
-                foreach (int category in Enum.GetValues(typeof(Const.CATEGORY_TYPE)))
-                {
-                    var entity = DeepCopyHelper.DeepCopy<CostMngTotal>(data);
-                    entity.year = Const.TARGET_YEAR;
-                    entity.category = category;
-                    entity.update_user = string.Concat(SystemInformation.ComputerName, "/", SystemInformation.UserName);
-                    entity.update_date = DateTime.Now;
-                    context.CostMngTotal.Add(entity);
-                }
-            }
-
-            ////---------------------------------------------------------
-            var target02 = from t in context.Divergence
-                           where t.year.Equals(srcYear)
-                           select t;
-            foreach (var data in target02.ToList())
-            {
-                var entity = DeepCopyHelper.DeepCopy<Divergence>(data);
+                //// 固定費データは実績データにはコピーしない
+                //foreach (int category in Enum.GetValues(typeof(Const.CATEGORY_TYPE)))
+                //{
+                var entity = DeepCopyHelper.DeepCopy<CostMngTotal>(data);
                 entity.year = Const.TARGET_YEAR;
+                entity.category = (int)Const.CATEGORY_TYPE.Budget;
                 entity.update_user = string.Concat(SystemInformation.ComputerName, "/", SystemInformation.UserName);
                 entity.update_date = DateTime.Now;
-                context.Divergence.Add(entity);
+                context.CostMngTotal.Add(entity);
+                //}
             }
+            logStr += string.Concat("CostMngTotal=", target01.Count(), "件 ");
+
+            ////---------------------------------------------------------
+            ////乖離幅データはコピーしない
+            //var target02 = from t in context.Divergence
+            //               where t.year.Equals(srcYear)
+            //               select t;
+            //foreach (var data in target02.ToList())
+            //{
+            //    var entity = DeepCopyHelper.DeepCopy<Divergence>(data);
+            //    entity.year = Const.TARGET_YEAR;
+            //    entity.update_user = string.Concat(SystemInformation.ComputerName, "/", SystemInformation.UserName);
+            //    entity.update_date = DateTime.Now;
+            //    context.Divergence.Add(entity);
+            //}
+            logStr += string.Concat("Divergence=", decimal.Zero, "件 ");
 
             ////---------------------------------------------------------
             var target03 = from t in context.Fare
@@ -136,6 +143,7 @@ namespace CostAccounting
                 entity.update_date = DateTime.Now;
                 context.Fare.Add(entity);
             }
+            logStr += string.Concat("Fare=", target03.Count(), "件 ");
 
             //---------------------------------------------------------
             var target04 = from t in context.Item
@@ -149,6 +157,7 @@ namespace CostAccounting
                 entity.update_date = DateTime.Now;
                 context.Item.Add(entity);
             }
+            logStr += string.Concat("Item=", target04.Count(), "件 ");
 
             //---------------------------------------------------------
             var target05 = from t in context.Machine
@@ -163,6 +172,7 @@ namespace CostAccounting
                 entity.update_date = DateTime.Now;
                 context.Machine.Add(entity);
             }
+            logStr += string.Concat("Machine=", target05.Count(), "件 ");
 
             //---------------------------------------------------------
             var target06 = from t in context.Material
@@ -177,6 +187,7 @@ namespace CostAccounting
                 entity.update_date = DateTime.Now;
                 context.Material.Add(entity);
             }
+            logStr += string.Concat("Material=", target06.Count(), "件 ");
 
             //---------------------------------------------------------
             var target07 = from t in context.Other
@@ -195,6 +206,7 @@ namespace CostAccounting
                     context.Other.Add(entity);
                 }
             }
+            logStr += string.Concat("Other=", target07.Count(), "件 ");
 
             //---------------------------------------------------------
             var target08 = from t in context.Product
@@ -213,6 +225,7 @@ namespace CostAccounting
                     context.Product.Add(entity);
                 }
             }
+            logStr += string.Concat("Product=", target08.Count(), "件 ");
 
             //---------------------------------------------------------
             var target09 = from t in context.ProductBlend
@@ -231,6 +244,7 @@ namespace CostAccounting
                     context.ProductBlend.Add(entity);
                 }
             }
+            logStr += string.Concat("ProductBlend=", target09.Count(), "件 ");
 
             //---------------------------------------------------------
             var target10 = from t in context.ProductCode
@@ -244,6 +258,7 @@ namespace CostAccounting
                 entity.update_date = DateTime.Now;
                 context.ProductCode.Add(entity);
             }
+            logStr += string.Concat("ProductCode=", target10.Count(), "件 ");
 
             //---------------------------------------------------------
             var target11 = from t in context.ProductContractor
@@ -262,6 +277,7 @@ namespace CostAccounting
                     context.ProductContractor.Add(entity);
                 }
             }
+            logStr += string.Concat("ProductContractor=", target11.Count(), "件 ");
 
             //---------------------------------------------------------
             var target12 = from t in context.ProductMachine
@@ -280,6 +296,7 @@ namespace CostAccounting
                     context.ProductMachine.Add(entity);
                 }
             }
+            logStr += string.Concat("ProductMachine=", target12.Count(), "件 ");
 
             //---------------------------------------------------------
             var target13 = from t in context.ProductMaterial
@@ -298,6 +315,7 @@ namespace CostAccounting
                     context.ProductMaterial.Add(entity);
                 }
             }
+            logStr += string.Concat("ProductMaterial=", target13.Count(), "件 ");
 
             //---------------------------------------------------------
             var target14 = from t in context.ProductMaterialsFare
@@ -316,6 +334,7 @@ namespace CostAccounting
                     context.ProductMaterialsFare.Add(entity);
                 }
             }
+            logStr += string.Concat("ProductMaterialsFare=", target14.Count(), "件 ");
 
             //---------------------------------------------------------
             var targe15 = from t in context.ProductPacking
@@ -334,6 +353,7 @@ namespace CostAccounting
                     context.ProductPacking.Add(entity);
                 }
             }
+            logStr += string.Concat("ProductPacking=", targe15.Count(), "件 ");
 
             //---------------------------------------------------------
             var target16 = from t in context.ProductPackingFare
@@ -352,6 +372,7 @@ namespace CostAccounting
                     context.ProductPackingFare.Add(entity);
                 }
             }
+            logStr += string.Concat("ProductPackingFare=", target16.Count(), "件 ");
 
             //---------------------------------------------------------
             var target17 = from t in context.ProductSupplier
@@ -367,9 +388,20 @@ namespace CostAccounting
                     entity.category = category;
                     entity.update_user = string.Concat(SystemInformation.ComputerName, "/", SystemInformation.UserName);
                     entity.update_date = DateTime.Now;
+
+                    // 実績データの売上にははコピーしない
+                    if (category.Equals((int)Const.CATEGORY_TYPE.Actual))
+                    {
+                        entity.month_01 = decimal.Zero; entity.month_02 = decimal.Zero; entity.month_03 = decimal.Zero;
+                        entity.month_04 = decimal.Zero; entity.month_05 = decimal.Zero; entity.month_06 = decimal.Zero;
+                        entity.month_07 = decimal.Zero; entity.month_08 = decimal.Zero; entity.month_09 = decimal.Zero;
+                        entity.month_10 = decimal.Zero; entity.month_11 = decimal.Zero; entity.month_12 = decimal.Zero;
+                    }
+
                     context.ProductSupplier.Add(entity);
                 }
             }
+            logStr += string.Concat("ProductSupplier=", target17.Count(), "件 ");
 
             //---------------------------------------------------------
             var target18 = from t in context.RowMaterial
@@ -384,6 +416,7 @@ namespace CostAccounting
                 entity.update_date = DateTime.Now;
                 context.RowMaterial.Add(entity);
             }
+            logStr += string.Concat("RowMaterial=", target18.Count(), "件 ");
 
             //---------------------------------------------------------
             var target19 = from t in context.Supplier
@@ -397,126 +430,152 @@ namespace CostAccounting
                 entity.update_date = DateTime.Now;
                 context.Supplier.Add(entity);
             }
+            logStr += string.Concat("Supplier=", target19.Count(), "件 ");
+
+            return logStr.TrimEnd(' ');
         }
 
         /*************************************************************
          * データ削除処理
          *************************************************************/
-        private void allDelete(CostAccountingEntities context)
+        private string allDelete(CostAccountingEntities context)
         {
+            string logStr = string.Empty;
+
             //---------------------------------------------------------
             var target01 = from t in context.CostMngTotal
                            where t.year.Equals(Const.TARGET_YEAR)
                            select t;
             context.CostMngTotal.RemoveRange(target01);
+            logStr += string.Concat("CostMngTotal=", target01.Count(), "件 ");
 
             //---------------------------------------------------------
             var target02 = from t in context.Divergence
                            where t.year.Equals(Const.TARGET_YEAR)
                            select t;
             context.Divergence.RemoveRange(target02);
+            logStr += string.Concat("Divergence=", target02.Count(), "件 ");
 
             //---------------------------------------------------------
             var target03 = from t in context.Fare
                            where t.year.Equals(Const.TARGET_YEAR)
                            select t;
             context.Fare.RemoveRange(target03);
+            logStr += string.Concat("Fare=", target03.Count(), "件 ");
 
             //---------------------------------------------------------
             var target04 = from t in context.Item
                            where t.year.Equals(Const.TARGET_YEAR)
                            select t;
             context.Item.RemoveRange(target04);
+            logStr += string.Concat("Item=", target04.Count(), "件 ");
 
             //---------------------------------------------------------
             var target05 = from t in context.Machine
                            where t.year.Equals(Const.TARGET_YEAR)
                            select t;
             context.Machine.RemoveRange(target05);
+            logStr += string.Concat("Machine=", target05.Count(), "件 ");
 
             //---------------------------------------------------------
             var target06 = from t in context.Material
                            where t.year.Equals(Const.TARGET_YEAR)
                            select t;
             context.Material.RemoveRange(target06);
+            logStr += string.Concat("Material=", target06.Count(), "件 ");
 
             //---------------------------------------------------------
             var target07 = from t in context.Other
                            where t.year.Equals(Const.TARGET_YEAR)
                            select t;
             context.Other.RemoveRange(target07);
+            logStr += string.Concat("Other=", target07.Count(), "件 ");
 
             //---------------------------------------------------------
             var target08 = from t in context.Product
                            where t.year.Equals(Const.TARGET_YEAR)
                            select t;
             context.Product.RemoveRange(target08);
+            logStr += string.Concat("Product=", target08.Count(), "件 ");
 
             //---------------------------------------------------------
             var target09 = from t in context.ProductBlend
                            where t.year.Equals(Const.TARGET_YEAR)
                            select t;
             context.ProductBlend.RemoveRange(target09);
+            logStr += string.Concat("ProductBlend=", target09.Count(), "件 ");
 
             //---------------------------------------------------------
             var target10 = from t in context.ProductCode
                            where t.year.Equals(Const.TARGET_YEAR)
                            select t;
             context.ProductCode.RemoveRange(target10);
+            logStr += string.Concat("ProductCode=", target10.Count(), "件 ");
 
             //---------------------------------------------------------
             var target11 = from t in context.ProductContractor
                            where t.year.Equals(Const.TARGET_YEAR)
                            select t;
             context.ProductContractor.RemoveRange(target11);
+            logStr += string.Concat("ProductContractor=", target11.Count(), "件 ");
 
             //---------------------------------------------------------
             var target12 = from t in context.ProductMachine
                            where t.year.Equals(Const.TARGET_YEAR)
                            select t;
             context.ProductMachine.RemoveRange(target12);
+            logStr += string.Concat("ProductMachine=", target12.Count(), "件 ");
 
             //---------------------------------------------------------
             var target13 = from t in context.ProductMaterial
                            where t.year.Equals(Const.TARGET_YEAR)
                            select t;
             context.ProductMaterial.RemoveRange(target13);
+            logStr += string.Concat("ProductMaterial=", target13.Count(), "件 ");
 
             //---------------------------------------------------------
             var target14 = from t in context.ProductMaterialsFare
                            where t.year.Equals(Const.TARGET_YEAR)
                            select t;
             context.ProductMaterialsFare.RemoveRange(target14);
+            logStr += string.Concat("Divergence=", target01.Count(), "件 ");
 
             //---------------------------------------------------------
             var targe15 = from t in context.ProductPacking
                           where t.year.Equals(Const.TARGET_YEAR)
                           select t;
             context.ProductPacking.RemoveRange(targe15);
+            logStr += string.Concat("ProductPacking=", targe15.Count(), "件 ");
 
             //---------------------------------------------------------
             var target16 = from t in context.ProductPackingFare
                            where t.year.Equals(Const.TARGET_YEAR)
                            select t;
             context.ProductPackingFare.RemoveRange(target16);
+            logStr += string.Concat("ProductPackingFare=", target16.Count(), "件 ");
 
             //---------------------------------------------------------
             var target17 = from t in context.ProductSupplier
                            where t.year.Equals(Const.TARGET_YEAR)
                            select t;
             context.ProductSupplier.RemoveRange(target17);
+            logStr += string.Concat("ProductSupplier=", target17.Count(), "件 ");
 
             //---------------------------------------------------------
             var target18 = from t in context.RowMaterial
                            where t.year.Equals(Const.TARGET_YEAR)
                            select t;
             context.RowMaterial.RemoveRange(target18);
+            logStr += string.Concat("RowMaterial=", target18.Count(), "件 ");
 
             //---------------------------------------------------------
             var target19 = from t in context.Supplier
                            where t.year.Equals(Const.TARGET_YEAR)
                            select t;
             context.Supplier.RemoveRange(target19);
+            logStr += string.Concat("Supplier=", target19.Count(), "件 ");
+
+            return logStr.TrimEnd(' ');
         }
     }
 }
